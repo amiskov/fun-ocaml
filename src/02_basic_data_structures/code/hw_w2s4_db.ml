ab8ef99e1467aad5923b367aa14aaca3c15ba1bd
Andrey Miskov <amiskov@gmail.com>
2019-11-21 18:42:01 +0300

    Add first 2 tasks

--- src/02_basic_data_structures/code/hw_w2s4_db.ml
+++ src/02_basic_data_structures/code/hw_w2s4_db.ml
@@ -65,14 +65,21 @@ let delete db contact =
   let (status, db, contact) = search db contact in
   if not status then (false, db, contact)
   else
+    let sorter a b =
+      (* Sort contacts array so nobodies will be after normal contacts *)
+      if (a.name = "" && b.name = "") then 0
+      else if a.name = "" then 1
+      else -1 in
     let cells i =
       if db.contacts.(i).name = contact.name then
         nobody
       else
         db.contacts.(i) in
+    let arr = (Array.init (Array.length db.contacts) cells) in
+    let _ = (Array.sort sorter arr) in
     let db' = {
       number_of_contacts = db.number_of_contacts - 1;
-      contacts = Array.init (Array.length db.contacts) cells
+      contacts = arr
     }
     in
     (true, db', contact);;
@@ -84,26 +91,45 @@ let engine db { code ; contact } =
   else if code = 2 then search db contact
   else (false, db, nobody);;
 
+let sveta = {name = "Sveta"; phone_number = (1,2,3,4) }
+let vasya = {name = "Vasya"; phone_number = (2,3,4,5) };;
+
+(* let kolya = {name = "Kolya"; phone_number = (3,4,5,6) }
+   let stepa = {name = "Stepa"; phone_number = (4,5,6,7) }
+   let vanya = {name = "Vanya"; phone_number = (5,6,7,8) }
+*)
+
+(* Initialize DB with 5 fields *)
+let db = make 3;;
+
+print_string "Insert Sveta\n";;
+let q1 = engine db { code = 0; contact = sveta };;
+
+print_string "Insert Vasya\n";;
+let q2 = let (_, db, _) = q1 in engine db { code = 0; contact = vasya };;
+
+print_string "Remove Sveta\n";;
+let q3 = let (_, db, _) = q2 in engine db { code = 1; contact = sveta };;
+
+print_string "Find Vasya\n";;
+let q4 = let (_, db, _) = q3 in engine db { code = 2; contact = vasya }
 
 (* Homework *)
-let proof_of_bug =
-  [| "Replace this string with your queries." |] ;;
+(* let proof_of_bug =
+   let sveta = {name = "Sveta"; phone_number = (1,2,3,4) } in
+   let vasya = {name = "Vasya"; phone_number = (2,3,4,5) } in
+   [|{ code = 0; contact = sveta }; 
+    { code = 0; contact = vasya }; 
+    { code = 1; contact = sveta };
+    { code = 2; contact = vasya}|]
+*)
 
-let delete db contact =
-  "Replace this string with your implementation." ;;
 
-let update db contact =
-  "Replace this string with your implementation." ;;
+(*  let delete db contact =
+    "Replace this string with your implementation." ;;
 
-let engine db { code ; contact } =
-  "Replace this string with your implementation." ;;
+    let update db contact =
+    "Replace this string with your implementation." ;;
 
-type query =
-  | Insert of contact
-  | Delete of contact
-  | Search of string
-
-let eng db query = match query with
-  | Insert contact -> insert db contact
-  | Delete contact -> delete db contact
-  | Search name -> search db contact
+    let engine db { code ; contact } =
+    "Replace this string with your implementation." ;; *)
